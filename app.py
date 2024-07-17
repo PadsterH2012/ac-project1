5029718d0f2b0345fff69b6bf0b4a83022564aee
Paddy <Paddy@test.com>
2024-07-17 19:47:27 +0000

    test

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from models import db
 from routes import init_app as init_routes
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -14,10 +16,12 @@ def create_app():
     app.config["GOOGLE_CONSUMER_SECRET"] = "your_google_client_secret"
     app.config["FACEBOOK_APP_ID"] = "your_facebook_app_id"
     app.config["FACEBOOK_APP_SECRET"] = "your_facebook_app_secret"
+    app.config['UPLOAD_FOLDER'] = 'static/avatars'
     
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'routes.index'
+    migrate.init_app(app, db)
 
     # Register routes
     init_routes(app)
@@ -31,3 +35,8 @@ def create_app():
         db.create_all()  # This line creates the database tables
 
     return app
+
+@create_app().cli.command("db_migrate")
+def db_migrate():
+    from migrations import add_provider_id_to_agent
+    add_provider_id_to_agent.upgrade()
